d4e2c91526ca1e6158243447b684b30ed72395bb
Thomas Christensen <31951107+Thom9521@users.noreply.github.com>
2018-04-07 19:00:40 +0200

    Update Opgave_1_1.java

--- src/Thom9521/Chapter_01/Opgave_1_1.java
+++ src/Thom9521/Chapter_01/Opgave_1_1.java
@@ -13,8 +13,8 @@ public class Opgave_1_1 {
 
         System.out.println("=");
 
-        System.out.println((1)+(2)+(3)+(4)+(5)+(6)+(7)+(8)+(9)+(10));
-        // Det er meget vigtigt at få styr på parenteserne.
+        System.out.println(1+2+3+4+5+6+7+8+9+10);
+      
 
 
     }
